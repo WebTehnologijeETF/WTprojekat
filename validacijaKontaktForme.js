f47c25002475420e5d96ae590230a08509ee6318
emrahdautbegovic <edautbegov1@etf.unsa.ba>
2015-08-05 15:15:58 +0200

    ajaxValidacija
    ajax validacija

--- validacijaKontaktForme.js
+++ validacijaKontaktForme.js
@@ -1,4 +1,4 @@
-function validImena() {
+function validForme() {
     var textboxIme = document.getElementById("ime");
     var slikaGreska = document.getElementById("greska");
     var porukaGreska = document.getElementById("greskaImena");
@@ -41,4 +41,33 @@ function validImena() {
     var res = mail.match(textboxIme.value);
     if (res == null)
         window.alert("ime mora biti sadrzano u e-mailu");
+
+    provjeraMjestaIOpcine();
+}
+
+function provjeraMjestaIOpcine() {
+    var mjesto = document.getElementById('mjesto').value;
+    var opcina = document.getElementById('opcina').value;
+    var servis = "http://zamger.etf.unsa.ba/wt/mjesto_opcina.php?opcina=" + opcina + "&mjesto=" + mjesto;
+    if (mjesto.length === 0) {
+        alert("Niste unijeli mjesto!");
+        return false;
+    }
+    if (opcina.length === 0) {
+        alert("Niste unijeli opcinu!");   
+        return false;
+    }    
+    var ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4 && ajax.status == 200) {
+            var odgovor = JSON.parse(ajax.responseText);
+            if (odgovor.hasOwnProperty('greska')) {                
+                alert(odgovor.greska);
+            }
+        }
+        if (ajax.readyState == 4 && ajax.status == 404)
+            document.innerHTML = stranica.toString();
+    }
+    ajax.open("GET", servis, true);
+    ajax.send();    
 }

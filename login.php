bd38b3ee4e44ef346f18990570cbfebd4f4cc12b
emrahdautbegovic <edautbegov1@etf.unsa.ba>
2015-08-29 11:35:36 +0200

    5.spirala

---
+++ login.php
@@ -0,0 +1,62 @@
+<?php
+    if(isset($_POST["btnLogin"])) {
+        $veza = new PDO("mysql:dbname=dekordb;host=localhost;charset=utf8", "dekor", "1DvaTri!");
+        $veza->exec("set names utf8");   
+        $user = $_POST['user'];
+        $pass = $_POST['pass'];
+        $pass = md5($pass);
+    
+        $upit = $veza->prepare("select * from korisnik where username=? and password=?");           
+        $upit->execute(array($user, $pass));           
+        if (!$upit) {
+            $greska = $veza->errorInfo();
+            print "SQL greška kod dobavljanja korisnika: ". $greska[2];
+            exit();
+        }
+         $rezultat = NULL;
+            foreach($upit as $value) {
+                $rezultat = $value;
+                break;
+            }
+        if($rezultat == NULL) echo "Administrator sa tim imenom ili lozinkom ne postoji !";
+        else{          
+            session_start();            
+            $_SESSION['username'] = $rezultat['username'];
+            $_SESSION['mail'] = $rezultat['mail'];
+            $_SESSION['tip'] = $rezultat['tip'];  
+            $_SESSION['korisnikid'] = $rezultat['id'];
+            header("Location: index.php"); /* Redirect browser */
+            exit();    
+    
+        }    
+    }          
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Login</title>
+        <link rel="stylesheet" type="text/css" href="stilindex.css">
+    </head>
+    <body>
+        <div id="login">
+            <h3>Prijava</h3>
+            <form action="login.php" method="post">
+                <table>
+                    <tr>
+                        <td>Username:</td>
+                        <td><input name="user"></td>
+                    </tr>
+                    <tr>
+                        <td>Password:</td>
+                        <td><input type="password" name="pass"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"><input type="submit" value="Prijava" name="btnLogin"></td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+    </body>
+</html>
